cbcfc3e4d6ca9669def931715da1da6d23f974c1
Lucas Alves <alveslucas@terceiros.sicredi.com.br>
2025-02-09 12:05:42 -0300

    Implementa padrão Singleton

--- creational-patterns/creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/conn/Connection.java
+++ creational-patterns/creational-patterns/src/main/java/br/com/cod3r/singleton/connectionPool/conn/Connection.java
@@ -6,17 +6,18 @@ private boolean inUse;
 	public Connection() {
 		inUse = false;
 	}
-	
-	public Object query(String sql) {
-		System.out.println("Running '" + sql + "' in Connection " + this);
-		return null;
-	}
-	
-	boolean isInUse() {
-		return inUse;
-	}
-	
-	void setInUse(boolean status) {
-		inUse = status;
-	}
+
+  public Object query(String sql) {
+    System.out.println("Running '" + sql + "' in Connection " + this);
+    return null;
+  }
+
+  boolean isInUse() {
+    return inUse;
+  }
+
+  void setInUse(boolean inUse) {
+    this.inUse = inUse;
+  }
+
 }
