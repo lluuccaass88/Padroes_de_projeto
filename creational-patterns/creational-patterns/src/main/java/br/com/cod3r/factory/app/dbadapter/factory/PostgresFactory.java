b4660b0818d2dff50bd3c6da2c6d7b8c949af88d
Lucas Alves <alveslucas@terceiros.sicredi.com.br>
2025-02-09 10:50:21 -0300

    Implementa padrão factory em db

---
+++ creational-patterns/creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/factory/PostgresFactory.java
@@ -0,0 +1,11 @@
+package br.com.cod3r.factory.app.dbadapter.factory;
+
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+
+public class PostgresFactory  implements DBFactory{
+  @Override
+  public DB getDatabase() {
+    return null;
+  }
+
+}
