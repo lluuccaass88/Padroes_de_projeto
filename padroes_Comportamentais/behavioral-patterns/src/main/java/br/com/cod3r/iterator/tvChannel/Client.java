eb218baf992b032dcb79cf524d051afd19a0c9ec
Lucas Alves <alveslucas@terceiros.sicredi.com.br>
2025-02-25 17:16:06 -0300

    Implementa padrão Iterator

--- padroes_Comportamentais/behavioral-patterns/src/main/java/br/com/cod3r/iterator/tvChannel/Client.java
+++ padroes_Comportamentais/behavioral-patterns/src/main/java/br/com/cod3r/iterator/tvChannel/Client.java
@@ -3,11 +3,11 @@ package br.com.cod3r.iterator.tvChannel;
 public class Client {
 
 	public static void main(String[] args) {
-		TV tv = new TV();
-		tv.searchAvaiableChannels();
+    TV tv = new TV();
+    tv.searchAvaiableChannels();
 
-		for(int i = 1; i < 30; i++) {
-			tv.accessChannel(i);
-		}
+    for(Integer channel: tv) {
+      System.out.println(String.format("Zapping at channel #%d", channel));
+    }
 	}
 }
